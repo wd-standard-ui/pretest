084d869f5aa478469c17b18294c2d4dc55d4ac79
wd-standard-ui <wd-standard@bwd.ac.th>
2025-10-02 20:19:29 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,4 @@
+// config.js — ตั้งค่าระบบ (แก้ให้ตรงโปรดักชันของพี่ที่รัก)
+export const LIFF_ID = "2006490627-xn8XaYD1"; // ตัวอย่าง: "2006490627-xn8XaYD1"
+export const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz-oZqlOmVTSc3Y8BUBe1oIElX8-LAYfaKmDAu0J0ofysmAEICDm96L3Up_dJmPJrLTTA/exec"; // ตัวอย่าง: "https://script.google.com/macros/s/XXX/exec"
+export const ALLOWED_ORIGIN = location.origin; // ใช้โดเมนปัจจุบัน
