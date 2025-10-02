871d65ae6a639a6a26aa4f1006d13f95d9dee608
wd-standard-ui <wd-standard@bwd.ac.th>
2025-10-02 20:34:51 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // config.js — ตั้งค่าระบบ (แก้ให้ตรงโปรดักชันของพี่ที่รัก)
-export const LIFF_ID = "YOUR_LIFF_ID_HERE"; // ตัวอย่าง: "2006490627-xn8XaYD1"
-export const SCRIPT_URL = "YOUR_GAS_WEB_APP_URL_HERE"; // ตัวอย่าง: "https://script.google.com/macros/s/XXX/exec"
+export const LIFF_ID = "2006490627-xn8XaYD1"; // ตัวอย่าง: "2006490627-xn8XaYD1"
+export const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz-oZqlOmVTSc3Y8BUBe1oIElX8-LAYfaKmDAu0J0ofysmAEICDm96L3Up_dJmPJrLTTA/exec"; // ตัวอย่าง: "https://script.google.com/macros/s/XXX/exec"
 export const ALLOWED_ORIGIN = location.origin; // ใช้โดเมนปัจจุบัน
